b1be7277c0704c38c3893886dd5f514ce2bcef16
Barry Fulk <bfulk@Barrys-MacBook-Pro.local>
2019-03-28 17:05:20 -0600

    file structure

---
+++ src/Components/Auth/Auth.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Auth extends Component {
+  render() {
+    return (
+      <div className="Auth">
+        <h1>Auth Component</h1>
+      </div>
+    );
+  }
+}
+
+export default Auth;
---
+++ src/Components/Post/Post.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+  render() {
+    return (
+      <div className="Post">
+        <h1>Post Component</h1>
+      </div>
+    );
+  }
+}
+
+export default Post;
